d6c440f73775a42db040994900df7c51c552ef44
tujun256 <tujun256@foxmail.com>
2024-08-14 07:00:17 +0800

    更新 diy-part1.sh

--- JDCloud-AX6000-Baili/diy-part1.sh
+++ JDCloud-AX6000-Baili/diy-part1.sh
@@ -15,6 +15,8 @@ echo "openwrt/target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt798
 
 cat ${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-glinet-gl-mt6000.dts
 
+sed -i 's/factory 0x0a/factory 0x2a/g ${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/mt7986/base-files/etc/board.d/02_network
+
 sed -i '/glinet,gl-mt6000)/ {n; s/lan1 lan2 lan3 lan4 lan5/lan1 lan2 lan3 lan4 lan5 lan6/}' target/linux/mediatek/mt7986/base-files/etc/board.d/02_network
 
 echo "openwrt/target/linux/mediatek/mt7986/base-files/etc/board.d/02_network"
@@ -26,3 +28,5 @@ sed -i 's/MT7986_ePAeLNA_EEPROM_AX6000.bin/MT7986_iPAiLNA_EEPROM_AX6000.bin/g' $
 echo "openwrt/target/linux/mediatek/mt7986/base-files/lib/preinit/90_extract_caldata"
 
 cat ${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/mt7986/base-files/lib/preinit/90_extract_caldata
+
+sed -i 's/factory 0x0a/factory 0x2a/g ${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/mt7986/base-files/etc/board.d/02_network
