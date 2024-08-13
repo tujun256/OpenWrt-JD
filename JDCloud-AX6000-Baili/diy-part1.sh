997a3ee9c2600af1eb9ac143707dbcabacd37383
tujun256 <tujun256@foxmail.com>
2024-08-14 07:46:57 +0800

    更新 diy-part1.sh

--- JDCloud-AX6000-Baili/diy-part1.sh
+++ JDCloud-AX6000-Baili/diy-part1.sh
@@ -18,7 +18,7 @@ cat ${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/files-5.4/arch/arm64/boot/
 
 sed -i 's/factory 0x0a/factory 0x2a/g' ${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/mt7986/base-files/etc/board.d/02_network
 
-sed -i '/glinet,gl-mt6000)/ {n; s/lan1 lan2 lan3 lan4 lan5/lan1 lan2 lan3 lan4 lan5 lan6/}' target/linux/mediatek/mt7986/base-files/etc/board.d/02_network
+sed -i '/glinet,gl-mt6000)/ {n; s/lan1 lan2 lan3 lan4 lan5/lan1 lan2 lan3 lan4 lan5 lan6/}' ${GITHUB_WORKSPACE}/openwrt/target/linux/mediatek/mt7986/base-files/etc/board.d/02_network
 
 echo "openwrt/target/linux/mediatek/mt7986/base-files/etc/board.d/02_network"
 
